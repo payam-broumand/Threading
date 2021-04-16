d2de5d8ca77e8c6d88a983e3c29242719f02f811
Payam <payam.broumandi@gmail.com>
2021-04-16 23:58:02 +0430

    add new ThreadStart and ParameterziedThreadStart to ThreadForm

--- Threading.AsyncDelegate/ThreadForm.Designer.cs
+++ Threading.AsyncDelegate/ThreadForm.Designer.cs
@@ -31,6 +31,9 @@
             this.btnStartProcess = new System.Windows.Forms.Button();
             this.btnStopProcess = new System.Windows.Forms.Button();
             this.txtProcessList = new System.Windows.Forms.TextBox();
+            this.btnParametrizedThreadStart = new System.Windows.Forms.Button();
+            this.txtParametrizedProcessList = new System.Windows.Forms.TextBox();
+            this.btnParametrizedThreadStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStartProcess
@@ -51,6 +54,7 @@
             this.btnStopProcess.TabIndex = 0;
             this.btnStopProcess.Text = "Stop Process";
             this.btnStopProcess.UseVisualStyleBackColor = true;
+            this.btnStopProcess.Click += new System.EventHandler(this.btnStopProcess_Click);
             // 
             // txtProcessList
             // 
@@ -61,15 +65,48 @@
             this.txtProcessList.Size = new System.Drawing.Size(361, 390);
             this.txtProcessList.TabIndex = 1;
             // 
+            // btnParametrizedThreadStart
+            // 
+            this.btnParametrizedThreadStart.Location = new System.Drawing.Point(392, 12);
+            this.btnParametrizedThreadStart.Name = "btnParametrizedThreadStart";
+            this.btnParametrizedThreadStart.Size = new System.Drawing.Size(133, 30);
+            this.btnParametrizedThreadStart.TabIndex = 0;
+            this.btnParametrizedThreadStart.Text = "Parametrized Thread";
+            this.btnParametrizedThreadStart.UseVisualStyleBackColor = true;
+            this.btnParametrizedThreadStart.Click += new System.EventHandler(this.btnParametrizedThreadStart_Click);
+            // 
+            // txtParametrizedProcessList
+            // 
+            this.txtParametrizedProcessList.Location = new System.Drawing.Point(392, 48);
+            this.txtParametrizedProcessList.Multiline = true;
+            this.txtParametrizedProcessList.Name = "txtParametrizedProcessList";
+            this.txtParametrizedProcessList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtParametrizedProcessList.Size = new System.Drawing.Size(361, 390);
+            this.txtParametrizedProcessList.TabIndex = 1;
+            // 
+            // btnParametrizedThreadStop
+            // 
+            this.btnParametrizedThreadStop.Location = new System.Drawing.Point(531, 12);
+            this.btnParametrizedThreadStop.Name = "btnParametrizedThreadStop";
+            this.btnParametrizedThreadStop.Size = new System.Drawing.Size(108, 30);
+            this.btnParametrizedThreadStop.TabIndex = 0;
+            this.btnParametrizedThreadStop.Text = "Stop Process";
+            this.btnParametrizedThreadStop.UseVisualStyleBackColor = true;
+            this.btnParametrizedThreadStop.Click += new System.EventHandler(this.btnParametrizedThreadStop_Click);
+            // 
             // ThreadForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 450);
+            this.ClientSize = new System.Drawing.Size(820, 450);
+            this.Controls.Add(this.txtParametrizedProcessList);
             this.Controls.Add(this.txtProcessList);
+            this.Controls.Add(this.btnParametrizedThreadStop);
             this.Controls.Add(this.btnStopProcess);
+            this.Controls.Add(this.btnParametrizedThreadStart);
             this.Controls.Add(this.btnStartProcess);
             this.Name = "ThreadForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ThreadForm";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -81,5 +118,8 @@
         private System.Windows.Forms.Button btnStartProcess;
         private System.Windows.Forms.Button btnStopProcess;
         private System.Windows.Forms.TextBox txtProcessList;
+        private System.Windows.Forms.Button btnParametrizedThreadStart;
+        private System.Windows.Forms.TextBox txtParametrizedProcessList;
+        private System.Windows.Forms.Button btnParametrizedThreadStop;
     }
 }
--- Threading.AsyncDelegate/ThreadForm.cs
+++ Threading.AsyncDelegate/ThreadForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,13 +13,109 @@ namespace Threading.AsyncDelegate
 {
     public partial class ThreadForm : Form
     {
+        private bool abortSecondThread, abortThiredThread;
         public ThreadForm()
         {
             InitializeComponent();
+            btnStartProcess.Enabled = true;
+            btnStopProcess.Enabled = true;
         }
 
         private void btnStartProcess_Click(object sender, EventArgs e)
         {
+            abortSecondThread = false;
+            txtProcessList.Clear();
+            if (!string.IsNullOrEmpty(Thread.CurrentThread.Name))
+                Thread.CurrentThread.Name = "Primary Thread";
+            Thread secondThread = new Thread(new ThreadStart(ProcessData));
+            txtProcessList.Text = $"Program running by {Thread.CurrentThread.Name} with ID {Thread.CurrentThread.ManagedThreadId}";
+            secondThread.Name = "Second Thread";
+            secondThread.Start();
+            txtProcessList.Text += $"{Environment.NewLine}All Done{Environment.NewLine}";
+            btnStartProcess.Enabled = false;
+            btnStopProcess.Enabled = true;
         }
+
+        private void ProcessData()
+        {
+            string secondThreadName = Thread.CurrentThread.Name;
+            for (int i = 0; i < 100; i++)
+            {
+                if (abortSecondThread) Thread.CurrentThread.Abort();
+                if (txtProcessList.InvokeRequired)
+                    this.Invoke(new Action(() => UpdateTextBox(i + 1, txtProcessList, secondThreadName)));
+                else
+                    UpdateTextBox(i + 1, txtProcessList, secondThreadName);
+                Thread.Sleep(500);
+            }
+
+            Thread thiredThread = new Thread(new ParameterizedThreadStart(ProcessSumNumbers));
+            if(!string.IsNullOrEmpty(thiredThread.Name))
+                thiredThread.Name = "Thired Thread";
+            thiredThread.Start(new Number(1, 10));
+        }
+
+        private void UpdateTextBox(int processNumber, TextBox textBox, string threadName)
+        {
+            textBox.Text += $"Process {processNumber} Done on {threadName}.{Environment.NewLine}";
+        }
+
+        private void btnStopProcess_Click(object sender, EventArgs e)
+        {
+            abortSecondThread = true;
+            btnStartProcess.Enabled = true;
+            btnStopProcess.Enabled = false;
+        } 
+
+        private void ProcessSumNumbers(object data)
+        {
+            Number number = (Number)data;
+            string thiredThreadName = Thread.CurrentThread.Name;
+            for (int i = number.From; i <= number.To; i++)
+            {
+                if (abortThiredThread) Thread.CurrentThread.Abort();
+                if (txtParametrizedProcessList.InvokeRequired)
+                    this.Invoke(new Action(() => UpdateTextBox(i + 1, txtParametrizedProcessList, thiredThreadName)));
+                else
+                    UpdateTextBox(i + 1, txtParametrizedProcessList, thiredThreadName);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void btnParametrizedThreadStart_Click(object sender, EventArgs e)
+        {
+            abortThiredThread = false;
+            txtParametrizedProcessList.Clear();
+            if (!string.IsNullOrEmpty(Thread.CurrentThread.Name))
+                Thread.CurrentThread.Name = "Primary Thread";
+            txtParametrizedProcessList.Text = $"Running program from {Thread.CurrentThread.Name} with ID {Thread.CurrentThread.ManagedThreadId}";
+            Thread thiredThread = new Thread(new ParameterizedThreadStart(ProcessSumNumbers));
+            thiredThread.Name = "Thired Thread";
+            thiredThread.Start(new Number(1, 10));
+            btnParametrizedThreadStart.Enabled = false;
+            btnParametrizedThreadStop.Enabled = true;
+        }
+
+        private void btnParametrizedThreadStop_Click(object sender, EventArgs e)
+        {
+            abortThiredThread = true;
+            btnParametrizedThreadStart.Enabled = true;
+            btnParametrizedThreadStop.Enabled = false;
+        }
+    }
+
+    public class Number
+    {
+        public int From { get; set; }
+
+        public int To { get; set; }
+
+        public Number(int op1, int op2)
+        {
+            this.From = op1;
+            this.To = op2;
+        }
+
+
     }
 }
