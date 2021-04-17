0e74afa34bbab41d475ac8259243b7406b73f61f
Payam <payam.broumandi@gmail.com>
2021-04-17 17:52:13 +0430

    add Task to running action in another thread to unlock UI thread

--- Threading.AsyncDelegate/TaskParallelLibraryForm.cs
+++ Threading.AsyncDelegate/TaskParallelLibraryForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,16 +20,23 @@ namespace Threading.AsyncDelegate
 
         private void btnStartTask_Click(object sender, EventArgs e)
         {
-            var numbers = Enumerable.Repeat(20, 10000000);
-            int sum = 0;
-            //foreach (var number in numbers)
-            //{
-            //    sum += number;
-            //}
+            // create new stans of task to run action in another thread
+            //Task task = new Task(CalculateAverage);
+            //task.Start();
 
+            // using task with factory method
+            Task.Factory.StartNew(CalculateAverage);
+        }
+
+        private void CalculateAverage()
+        {
+            var numbers = Enumerable.Repeat(20, 100000000);
+            int sum = 0;
             Parallel.ForEach(numbers, number =>
             {
                 sum += number;
+                Invoke(new Action(() => txtTaskNumbers.Text += $"{sum}{Environment.NewLine}"));
+                Thread.Sleep(50);
             });
 
             MessageBox.Show("Average of numbers: " + (sum / numbers.Count()));
