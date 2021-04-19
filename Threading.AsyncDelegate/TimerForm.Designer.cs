b3cd53448982ba394feae793272ffd2436b4d03a
Payam <payam.broumandi@gmail.com>
2021-04-19 12:49:04 +0430

    initial TimerForm

---
+++ Threading.AsyncDelegate/TimerForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Threading.AsyncDelegate
+{
+    partial class TimerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnStartTimer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(91, 85);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(472, 138);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "label1";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStartTimer
+            // 
+            this.btnStartTimer.Location = new System.Drawing.Point(266, 226);
+            this.btnStartTimer.Name = "btnStartTimer";
+            this.btnStartTimer.Size = new System.Drawing.Size(110, 43);
+            this.btnStartTimer.TabIndex = 1;
+            this.btnStartTimer.Text = "Start Timer";
+            this.btnStartTimer.UseVisualStyleBackColor = true;
+            // 
+            // TimerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 371);
+            this.Controls.Add(this.btnStartTimer);
+            this.Controls.Add(this.lblTimer);
+            this.Name = "TimerForm";
+            this.Text = "TimerForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnStartTimer;
+    }
+}
